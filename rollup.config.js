c4535aa253b5d8f4b6733106ecf4eb8d198722c3
Cirizzzz <117946522+Cirizzzz@users.noreply.github.com>
2022-11-29 04:08:48 +0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,10 @@ import scss from 'rollup-plugin-scss';
 export default {
   input: 'src/index.js',
   output: {
-    file: 'project/output.js',
-   
+    file: 'project/output.js'
   },
   plugins: [
     scss({
-      
       output: "project/styles/ciri.css",
     })
   ]
